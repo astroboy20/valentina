c0c918f3b5c142873756a7f3aafff4b59abc75cc
astroboy20 <tolulopeakinkunmi7@gmail.com>
2025-02-09 04:31:25 +0100

    fix: update import for useRouter to use next/navigation

--- src/app/verify-email/container/verify-email.tsx
+++ src/app/verify-email/container/verify-email.tsx
@@ -1,7 +1,7 @@
 import { Logo_Small } from "@/assets";
 import { Button } from "@/components/ui/button";
 import { ArrowLeft } from "lucide-react";
-import { useRouter } from "next/router";
+import { useRouter } from "next/navigation";
 import React from "react";
 
 
