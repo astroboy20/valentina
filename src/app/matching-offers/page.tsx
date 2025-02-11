bfeae79bc75e6281292b7b8b13578e73082092f9
astroboy20 <tolulopeakinkunmi7@gmail.com>
2025-02-11 22:00:47 +0100

    feat: wrap Offers component in ProtectedRoute for access control

--- src/app/matching-offers/page.tsx
+++ src/app/matching-offers/page.tsx
@@ -1,10 +1,11 @@
+import { ProtectedRoute } from "@/provider/protected-route/protected-route";
 import { Match } from "./container/match";
 import { Offers } from "./container/offers";
 
-export default function Page(){
-    return(
-        <>
-        <Offers/>
-        </>
-    )
+export default function Page() {
+  return (
+    <ProtectedRoute>
+      <Offers />
+    </ProtectedRoute>
+  );
 }
